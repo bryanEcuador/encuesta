cc93fd55a2ab3e095064d54b843ec1bde317d7ca
Bryan <bryansilva021@hotmail.com>
2018-12-08 14:29:23 -0500

    configuracion de roles y permisos

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Caffeinated\Shinobi\ShinobiServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
